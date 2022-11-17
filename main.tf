9acb7913299b71eb8fbb2d5780b652217261f37c
folarin <folami25@gmail.com>
2022-11-17 23:42:26 +0100

    created a variable file and also populated the aws instance block with values from the .tfstate file

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
 resource "aws_instance" "webserver" {
-#   (resource arguments go below)
+#   (enter the values below once the import becomes successful)
+ami = "ami-08c40ec9ead489470"
+instance_type = "t2.micro"
+key_name = "server_key"
+vpc_security_group_ids = var.vpc_ids
 }
---
+++ variable.tf
@@ -0,0 +1,6 @@
+variable "vpc_ids" {
+  default = [
+              "sg-0423f3195c3590da4",
+              "sg-063f4d98acb553fdf"
+            ]
+}
