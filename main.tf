fdbda83ff63db9befe52692751a5d977533bc735
folarin <folami25@gmail.com>
2022-11-17 23:44:35 +0100

    successfully configured the imported instance.

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "webserver" {
-#   (enter the values below once the import becomes successful)
-ami = "ami-08c40ec9ead489470"
-instance_type = "t2.micro"
-key_name = "server_key"
-vpc_security_group_ids = var.vpc_ids
+  #   (enter the values below once the import becomes successful)
+  ami                    = "ami-08c40ec9ead489470"
+  instance_type          = "t2.micro"
+  key_name               = "server_key"
+  vpc_security_group_ids = var.vpc_ids
 }
